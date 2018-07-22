4886188b3cb0e83147dd98aeb804c5309fb3bfcc
Cgd15 <40788436+Cgd15@users.noreply.github.com>
2018-07-22 18:59:15 +0200

    Add files via upload

---
+++ js/reproductor.js
@@ -0,0 +1,179 @@
+$(window).on("load",function() {
+  				alert("Funcionamiento: 1. Selecciona la canción / 2. Dale al play")
+});
+
+$(document).ready(function() {
+  	$("#chihiro").click(function() {
+  		document.getElementById('portada').src = "img/chihiro.jpg";
+  		document.getElementById('rep').src = "BSO/chihiro.mp3";
+  		document.getElementById('titulo').innerHTML = "One summer's day";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#castilloambulante").click(function() {
+  		document.getElementById('portada').src = "img/castilloambulante.jpg";
+  		document.getElementById('rep').src = "BSO/castilloambulante.mp3";
+  		document.getElementById('titulo').innerHTML = "Merry go round of life";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#totoro").click(function() {
+  		document.getElementById('portada').src = "img/totoro.jpg";
+  		document.getElementById('rep').src = "BSO/wind.mp3";
+  		document.getElementById('titulo').innerHTML = "Path of the wind";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#mononoke").click(function() {
+  		document.getElementById('portada').src = "img/mononoke.jpg";
+  		document.getElementById('rep').src = "BSO/mononoke.mp3";
+  		document.getElementById('titulo').innerHTML = "Legend of Ashitaka";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#arrietty").click(function() {
+  		document.getElementById('portada').src = "img/arrietty.jpg";
+  		document.getElementById('rep').src = "BSO/arrietty.mp3";
+  		document.getElementById('titulo').innerHTML = "Arrietty's song";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#luciernagas").click(function() {
+  		document.getElementById('portada').src = "img/ltdll.jpg";
+  		document.getElementById('rep').src = "BSO/luciernagas.mp3";
+  		document.getElementById('titulo').innerHTML = "Ending theme";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#porco").click(function() {
+  		document.getElementById('portada').src = "img/pr.jpg";
+  		document.getElementById('rep').src = "BSO/porco.mp3";
+  		document.getElementById('titulo').innerHTML = "The bygone days";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#nicky").click(function() {
+  		document.getElementById('portada').src = "img/nladb.jpg";
+  		document.getElementById('rep').src = "BSO/nicky.mp3";
+  		document.getElementById('titulo').innerHTML = "A town with an ocean view";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#terramar").click(function() {
+  		document.getElementById('portada').src = "img/terramar.jpg";
+  		document.getElementById('rep').src = "BSO/terramar.mp3";
+  		document.getElementById('titulo').innerHTML = "Teru no Uta";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#amapolas").click(function() {
+  		document.getElementById('portada').src = "img/colina.jpg";
+  		document.getElementById('rep').src = "BSO/colina.mp3";
+  		document.getElementById('titulo').innerHTML = "Sayonara no Natsu";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#yamada").click(function() {
+  		document.getElementById('portada').src = "img/yamada.jpg";
+  		document.getElementById('rep').src = "BSO/yamada.mp3";
+  		document.getElementById('titulo').innerHTML = "Quit being alone";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#nausicaa").click(function() {
+  		document.getElementById('portada').src = "img/n_p.jpg";
+  		document.getElementById('rep').src = "BSO/nausicaa.mp3";
+  		document.getElementById('titulo').innerHTML = "Requiem";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#recuerdos").click(function() {
+  		document.getElementById('portada').src = "img/rda.jpg";
+  		document.getElementById('rep').src = "BSO/ayer.mp3";
+  		document.getElementById('titulo').innerHTML = "Main theme";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#pompoko").click(function() {
+  		document.getElementById('portada').src = "img/pompoko.png";
+  		document.getElementById('rep').src = "BSO/pompoko.mp3";
+  		document.getElementById('titulo').innerHTML = "Genki song";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#ponyo").click(function() {
+  		document.getElementById('portada').src = "img/ponyo.jpg";
+  		document.getElementById('rep').src = "BSO/ponyo.mp3";
+  		document.getElementById('titulo').innerHTML = "Mother of the sea";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#mar").click(function() {
+  		document.getElementById('portada').src = "img/peem.jpg";
+  		document.getElementById('rep').src = "BSO/mar.mp3";
+  		document.getElementById('titulo').innerHTML = "Ending theme";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#cielo").click(function() {
+  		document.getElementById('portada').src = "img/eceec-b.jpg";
+  		document.getElementById('rep').src = "BSO/cielo.mp3";
+  		document.getElementById('titulo').innerHTML = "Kimi wo Nosete";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#haru").click(function() {
+  		document.getElementById('portada').src = "img/haru.jpg";
+  		document.getElementById('rep').src = "BSO/haru.mp3";
+  		document.getElementById('titulo').innerHTML = "Kaze ni Naru";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#kaguya").click(function() {
+  		document.getElementById('portada').src = "img/kaguya.jpg";
+  		document.getElementById('rep').src = "BSO/kaguya.mp3";
+  		document.getElementById('titulo').innerHTML = "Astragalus grass";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#viento").click(function() {
+  		document.getElementById('portada').src = "img/viento.jpg";
+  		document.getElementById('rep').src = "BSO/viento.mp3";
+  		document.getElementById('titulo').innerHTML = "A journey";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#susurros").click(function() {
+  		document.getElementById('portada').src = "img/sdc.jpg";
+  		document.getElementById('rep').src = "BSO/susurros.mp3";
+  		document.getElementById('titulo').innerHTML = "Country roads";
+  	});
+ });
+
+$(document).ready(function() {
+  	$("#marnie").click(function() {
+  		document.getElementById('portada').src = "img/marnie.jpg";
+  		document.getElementById('rep').src = "BSO/marnie.mp3";
+  		document.getElementById('titulo').innerHTML = "Fine on the outside";
+  	});
+ });
